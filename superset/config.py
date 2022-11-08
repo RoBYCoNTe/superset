27f7a0b08625d852d1184eee80868149321cdcc5
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2022-11-08 15:39:21 +0100

    fix: added prefix middleware

--- superset/config.py
+++ superset/config.py
@@ -1457,8 +1457,25 @@ class ReverseProxied(object):
             environ['wsgi.url_scheme'] = scheme
         return self.app(environ, start_response)
 
-
 ADDITIONAL_MIDDLEWARE = [ReverseProxied, ]
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix='analytics'):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+ADDITIONAL_MIDDLEWARE = [PrefixMiddleware, ]
+
 
 # -------------------------------------------------------------------
 # *                WARNING:  STOP EDITING  HERE                    *
