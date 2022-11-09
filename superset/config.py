17efef538b0d5dc15d9c8d6860cab833ac719af1
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2022-11-09 11:49:41 +0100

    fix: removed unused middleware

--- superset/config.py
+++ superset/config.py
@@ -1458,23 +1458,6 @@ class ReverseProxied(object):
         return self.app(environ, start_response)
 
 ADDITIONAL_MIDDLEWARE = [ReverseProxied, ]
-class PrefixMiddleware(object):
-
-    def __init__(self, app, prefix='analytics'):
-        self.app = app
-        self.prefix = prefix
-
-    def __call__(self, environ, start_response):
-
-        if environ['PATH_INFO'].startswith(self.prefix):
-            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
-            environ['SCRIPT_NAME'] = self.prefix
-            return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])
-            return ["This url does not belong to the app.".encode()]
-
-ADDITIONAL_MIDDLEWARE = [PrefixMiddleware, ]
 
 
 # -------------------------------------------------------------------
