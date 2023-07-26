658fe735b5bc3c06bfdee2290a33ccf4e1732e7f
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2023-07-26 16:16:36 +0200

    dev: added ARESS 4

--- superset/config.py
+++ superset/config.py
@@ -542,6 +542,14 @@ EXTRA_CATEGORICAL_COLOR_SCHEMES: List[Dict[str, Any]] = [
         "label": "AReSS 3",
         "isDefault": False,
         "colors": ['#fff5f0', '#ebc9cb', '#d397aa', '#c45773', '#bc2c4f']
+    },
+    {
+        "id": "AReSS 4",
+        "description": "AReSS 3",
+        "label": "AReSS 3",
+        "isDefault": False,
+        "colors": ['#f44336','#e81e63','#9c27b0','#673ab7', '#3f51b5','#2196f3','#03a9f4','#00bcd4',
+                   '#009688','#4caf50','#8bc34a','#cddc39','#ffeb3b','#ffc107','#ff9800','#ff5722','#795548','#9e9e9e','#607d8b','#000000']
     }
 ]
 
@@ -601,6 +609,14 @@ EXTRA_SEQUENTIAL_COLOR_SCHEMES: List[Dict[str, Any]] = [
         "label": "AReSS 3",
         "isDefault": False,
         "colors": ['#fff5f0', '#ebc9cb', '#d397aa', '#c45773', '#bc2c4f']
+    },
+    {
+        "id": "AReSS 4",
+        "description": "AReSS 3",
+        "label": "AReSS 3",
+        "isDefault": False,
+        "colors": ['#f44336','#e81e63','#9c27b0','#673ab7', '#3f51b5','#2196f3','#03a9f4','#00bcd4',
+                   '#009688','#4caf50','#8bc34a','#cddc39','#ffeb3b','#ffc107','#ff9800','#ff5722','#795548','#9e9e9e','#607d8b','#000000']
     }
 ]
 
